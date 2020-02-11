6fa8f1d42ca5bd73bb1f8f4cf46bc098a821aed8
rhythmbhiwani <rhythmbhiwani@gmail.com>
2020-02-12 00:00:40 +0530

    Project Completed!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ask = (Button) findViewById(R.id.askButton);
+
+        final TextView output = (TextView) findViewById(R.id.output);
+
+        final int[] outArray = {R.string.out1,
+                R.string.out2,
+                R.string.out3};
+
+        ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random ran = new Random();
+                int num = ran.nextInt(3);
+                output.setText(outArray[num]);
+            }
+        });
+
+
     }
 }
